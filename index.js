03025beea91106bd2614746ab632b35799e5a11a
Muhammod Abul Bashar <45920385+Bashar12345@users.noreply.github.com>
2022-04-01 00:02:58 +0600

    init

--- index.js
+++ index.js
@@ -3,6 +3,10 @@
 // cy=eur
 // https://api.coindesk.com/v1/bpi/currentprice/eur.json
 
+// fetch('http://example.com/movies.json')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
 import fetch from "node-fetch";
 import express from 'express'
 //const express = require('express');
@@ -12,9 +16,7 @@ route.use(express.json())
 
 const data =[{},{}]
 
-// fetch('http://example.com/movies.json')
-//   .then(response => response.json())
-//   .then(data => console.log(data));
+
 
 
 route.get('/getBitcoinInfo',async(req,res)=>{
@@ -42,7 +44,10 @@ res.json(response);
 
 	// res.send("hello World");
 });
-route.get('/getBitcoinInfo/currentprice/:currency_type',async(req,res)=>{
+
+
+
+route.get('/getBitcoinInfo/currentPrice/:currency_type',async(req,res)=>{
 
 	const url='https://api.coindesk.com/v1/bpi/currentprice/eur.json/'
 
@@ -53,20 +58,38 @@ route.get('/getBitcoinInfo/currentprice/:currency_type',async(req,res)=>{
 	.then(res => res.json())
 	.catch(e => {
 		console.error({
-		'massages':'vhul hoise',
+		'massages':'Data ashe naaaaa!!!',
 		error: e,
 	});
 });
 
 //const currency_array 
-const data = Object.values(response.bpi['USD']);
+const currency_code = req.params.currency_type
 
+const data_set = Object.values(response['bpi']);
+
+
+let results = [];
+for (let key in data_set) {
+	console.log(data_set[key])
+	if (data_set[key].code==='USD'){
+		results.push(data_set[key]);
+	}
+}
+
+
+
+
+console.log(results);
 //const some =data.keys(String(req.params.currency_type))
 //object theke aaray bair korte hobe
 
+//console.log(data_set);
+res.json(results)
+//console.log(data);
 
-res.json(data);
-// console.log(typeof(some))
+//res.json(data);
+
 
 //  const  bicoin_price = data.find(r => r.bpi===String(req.params.currency_type));
 
@@ -82,7 +105,7 @@ res.json(data);
 
 
 
-	 res.send("hello World");
+	 //res.send("hello World");
 });
 const port = process.env.PORT || 3000;
 route.listen(port, ()=> console.log(`Listening on port ${port}`))
